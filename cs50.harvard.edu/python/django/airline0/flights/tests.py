f75dc11ffe40dd6bb9ddb08d99d07fa5823a5869
Mohammed Hayath <mdhayath64@gmail.com>
2021-06-05 00:50:18 +0530

    fixed

--- cs50.harvard.edu/python/django/airline0/flights/tests.py
+++ cs50.harvard.edu/python/django/airline0/flights/tests.py
@@ -59,9 +59,10 @@ class FlightTestCase(TestCase):
 
     def test_invalid_flight_page(self):
         max_id = Flight.objects.all().aggregate(Max("id"))["id__max"]
-
+        print(f"id - {max_id}")
         c = Client()
-        response = c.get(f"/flights/{max_id }")
+        response = c.get(f"/flights/{max_id}/")
+        print(f"respones{response}")
         self.assertEqual(response.status_code, 404)
 
     def test_flight_page_passengers(self):
